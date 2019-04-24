6fe5ace58963ffaed3a93b61c9d89ac08ba6f005
hytonwons <hytonwons@163.com>
2019-04-24 10:45:36 -0700

    Added test cases, Added progress summary for the week of 4/22/2019...

--- gsvd/test/alanTest.jl
+++ gsvd/test/alanTest.jl
@@ -1,15 +1,20 @@
 Base.show(io::IO, x::Union{Float64,Float32}) = Base.Grisu._show(io, x, Base.Grisu.SHORTEST, 0, true, false)
 m1,m2,n,r,rA,rB =2,2,3,2,1,2
 v=[0,0,0]
-trials = 200_000
+# trials = 200_000
+trials = 100
 for i = 1:trials
 
  E = randn(r,n)
  A = randn(m1,rA)*randn(rA,r)*E
  B = randn(m2,rB)*randn(rB,r)*E
  try
-       U,V,Q,C,S,R =gsvd(A,B,1)
-       v[size(C,2)] +=1
+       U,V,Q,C,S,R = gsvd(A,B,1)
+       if size(C,2) == 3
+           show(A);show(B);show(C)
+       end
+       # v[size(C,2)] +=1
+
    catch
        println("Found an LAPACK Bug on trial #$i")
        show(A);show(B)
@@ -17,5 +22,5 @@ for i = 1:trials
    end
 
 end
-println("% of time, C is 2x2: ", v[2]*100 ./ trials)
-println("% of time, C is 2x3: ", v[3]*100 ./ trials)
+# println("% of time, C is 2x2: ", v[2]*100 ./ trials)
+# println("% of time, C is 2x3: ", v[3]*100 ./ trials)
--- gsvd/test/test.jl
+++ gsvd/test/test.jl
@@ -1,11 +1,11 @@
 # This function tests the time performance
-# and stability of Quotient Singular Value Decomposition (QSVD)
+# and stability of Generalized Singular Value Decomposition (GSVD)
 #
 # First, we generate a m by n random matrix A and p by n random matrix B,
-# Create a copy of A and B, respectively (A and B will be overwritten)
+# Create a copy of A and B, respectively (original A and B will be overwritten)
 #
-# Then, we call qsvd function, return U, V, Q, C, S, R.
-# Calculate and print time elapsed for computing QSVD.
+# Then, we call gsvd function, return U, V, Q, C, S, R, k, l.
+# Calculate and print time elapsed for computing GSVD.
 #
 # Compute:
 #
@@ -45,6 +45,7 @@ function test(m, n, p)
     println("orthog_q: ", orthog_q)
 end
 
+# Example from Julia v1.1.0 documentation
 function test0()
     A = [1. 0.; 0. -1.]
     B = [0. 1.; 1. 0.]
@@ -67,6 +68,7 @@ function test0()
     U, V, Q, C, S, R, A_, B_
 end
 
+# Example 1 from Ch.4 of Jenny's Thesis
 function test1()
     A  = [1.0 2 1 0; 2 3 1 1; 3 4 1 2;4 5 1 3;5 6 1 4]
     A_ = copy(A)
@@ -88,6 +90,8 @@ function test1()
     println("orthog_q: ", orthog_q)
 end
 
+# Example 2 from Ch.4 of Jenny's Thesis
+# It turns out this example is wrong
 function test2()
     A = [1.0 2 1 0;2 3 1 1;3 4 1 2]
     A_ = copy(A)
@@ -109,10 +113,11 @@ function test2()
     println("orthog_q: ", orthog_q)
 end
 
+# Example 1 from MATLAB 2019b documentation
 function test3()
-    A = [1.0 2 1 0;-2 3 1 1;3 4 1 2]
+    A = [1.0 6 11;2 7 12;3 6 13;4 9 14;5 10 15]
     A_ = copy(A)
-    B = [4.0 5 1 3;5 6 1 4;6 7 1 5;7 1 -6 13]
+    B = [8.0 1 6;3 5 7;4 9 2]
     B_ = copy(B)
     m,n = size(A)
     p = size(B)[1]
@@ -131,10 +136,11 @@ function test3()
     U, V, Q, C, S, R
 end
 
+# Example 2 from MATLAB 2019b documentation
 function test4()
-    A = [1.0 2 1 0;-2 3 1 1;3 4 1 2]
+    A = [1.0 4 7 10 13;2 5 8 11 14;3 6 9 12 15]
     A_ = copy(A)
-    B = [4.0 5 1 3;5 -6 1 4;6 7 1 5;7 1 -6 13]
+    B = [17.0 24 1 8 15;23 5 7 14 16;4 6 13 20 22;10 12 19 21 3;11 19 25 2 9]
     B_ = copy(B)
     m,n = size(A)
     p = size(B)[1]
@@ -153,6 +159,7 @@ function test4()
     U, V, Q, C, S, R
 end
 
+# Example from Ch. 93 LAPACK of Handbook of Linear Algebra, 2nd Edition
 function test5()
     A = [1.0 2 3 1 5;0 3 2 0 2;1 0 2 1 0;0 2 3 0 -1;1 0 2 1 1;0 2 1 0 1]
     A_ = copy(A)
@@ -160,22 +167,25 @@ function test5()
     B_ = copy(B)
     m,n = size(A)
     p = size(B)[1]
-    U, V, Q, alpha, beta, R, k, l = gsvd(A, B, 0)
-    # e = eps(Float64)
-    # res_a = norm(U'*A_*Q - C*R, 1)/(max(m,n)*norm(A_, 1)*e)
-    # res_b = norm(V'*B_*Q - S*R, 1)/(max(m,n)*norm(B_, 1)*e)
-    # orthog_u = norm(I - U'*U, 1)/(m*e)
-    # orthog_v = norm(I - V'*V, 1)/(p*e)
-    # orthog_q = norm(I - Q'*Q, 1)/(n*e)
-    # println("res_a: ", res_a)
-    # println("res_b: ", res_b)
-    # println("orthog_u: ", orthog_u)
-    # println("orthog_v: ", orthog_v)
-    # println("orthog_q: ", orthog_q)
-    U, V, Q, alpha, beta, R, k, l
+    U, V, Q, C, S, R, k, l = gsvd(A, B, 1)
+    e = eps(Float64)
+    res_a = norm(U'*A_*Q - C*R, 1)/(max(m,n)*norm(A_, 1)*e)
+    res_b = norm(V'*B_*Q - S*R, 1)/(max(m,n)*norm(B_, 1)*e)
+    orthog_u = norm(I - U'*U, 1)/(m*e)
+    orthog_v = norm(I - V'*V, 1)/(p*e)
+    orthog_q = norm(I - Q'*Q, 1)/(n*e)
+    println("res_a: ", res_a)
+    println("res_b: ", res_b)
+    println("orthog_u: ", orthog_u)
+    println("orthog_v: ", orthog_v)
+    println("orthog_q: ", orthog_q)
+    U, V, Q, C, S, R, k, l
 end
 
-function test6()
+# Example from Ch. 93 LAPACK of Handbook of Linear Algebra, 2nd Edition
+# Same example from "A new preprocessing algorithm for the computation of
+# the generalized singular value decomposition"
+function test5()
     A = [1.0 2 3 1 5;0 3 2 0 2;1 0 2 1 0;0 2 3 0 -1;1 0 2 1 1;0 2 1 0 1]
     A_ = copy(A)
     B = [1.0 -2 2 1 1;0 3 0 0 0;1 -2 2 1 1;0 2 0 0 0;2 -4 4 2 2;1 3 2 1 1]
@@ -183,16 +193,16 @@ function test6()
     m,n = size(A)
     p = size(B)[1]
     U, V, Q, C, S, R, k, l = gsvd(A, B, 1)
-    # e = eps(Float64)
-    # res_a = norm(U'*A_*Q - C*R, 1)/(max(m,n)*norm(A_, 1)*e)
-    # res_b = norm(V'*B_*Q - S*R, 1)/(max(m,n)*norm(B_, 1)*e)
-    # orthog_u = norm(I - U'*U, 1)/(m*e)
-    # orthog_v = norm(I - V'*V, 1)/(p*e)
-    # orthog_q = norm(I - Q'*Q, 1)/(n*e)
-    # println("res_a: ", res_a)
-    # println("res_b: ", res_b)
-    # println("orthog_u: ", orthog_u)
-    # println("orthog_v: ", orthog_v)
-    # println("orthog_q: ", orthog_q)
+    e = eps(Float64)
+    res_a = norm(U'*A_*Q - C*R, 1)/(max(m,n)*norm(A_, 1)*e)
+    res_b = norm(V'*B_*Q - S*R, 1)/(max(m,n)*norm(B_, 1)*e)
+    orthog_u = norm(I - U'*U, 1)/(m*e)
+    orthog_v = norm(I - V'*V, 1)/(p*e)
+    orthog_q = norm(I - Q'*Q, 1)/(n*e)
+    println("res_a: ", res_a)
+    println("res_b: ", res_b)
+    println("orthog_u: ", orthog_u)
+    println("orthog_v: ", orthog_v)
+    println("orthog_q: ", orthog_q)
     U, V, Q, C, S, R, k, l
 end
