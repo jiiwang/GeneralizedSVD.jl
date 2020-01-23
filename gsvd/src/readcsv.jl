9df97801d2e8e2d6c44599ed45f768a20ff37be9
Ji Wang <hytonwons@campus-013-233.ucdavis.edu>
2020-01-23 15:03:08 -0800

    Examples(full rank/not full rank) added, different cases discussed...

--- gsvd/src/readcsv.jl
+++ gsvd/src/readcsv.jl
@@ -1,6 +1,6 @@
 using DelimitedFiles
 
-A = readdlm("a5.csv", ',', Float64)
-B = readdlm("b5.csv", ',', Float64)
+A = readdlm("examples/non-full-rank/A1.csv", ',', Float64)
+B = readdlm("examples/non-full-rank/B1.csv", ',', Float64)
 
 # U, V, Q, C, S, R, k, l = gsvd(A, B, 1);
